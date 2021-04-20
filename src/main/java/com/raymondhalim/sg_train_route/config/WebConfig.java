c251be41948cb0de6be47fbc4eb95395a044adaf
raymondhalim <raymondhalimdotcom@gmail.com>
2021-04-20 20:53:30 +0800

    Update config.

--- src/main/java/com/raymondhalim/sg_train_route/config/WebConfig.java
+++ src/main/java/com/raymondhalim/sg_train_route/config/WebConfig.java
@@ -3,14 +3,12 @@ package com.raymondhalim.sg_train_route.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
-import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -29,7 +27,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/")
                 .setCachePeriod(0)
-                .addResourceLocations("classpath:/static/index.html")
+                .addResourceLocations("classpath:/static/")
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver() {
                     @Override
